f6e8c0097428da4b87dfb53de6e88d21a58508ee
Georgy F <60139708+geofom@users.noreply.github.com>
2021-03-19 08:46:09 +0100

    Update code.js

---
+++ api/code.js
@@ -0,0 +1,17 @@
+const request = new XMLHttpRequest()
+
+request.open("GET", "https://binaryjazz.us/wp-json/genrenator/v1/story/")
+
+request.onload = function(){
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach((movie) => {
+        console.log(movie)
+      })
+    } else {
+      console.log('error')
+    }
+}
+
+request.send()
