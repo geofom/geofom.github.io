478d125ca898c290761a11e054990a84971ef389
Georgy F <60139708+geofom@users.noreply.github.com>
2021-03-23 13:45:12 +0100

    Update code.js

--- api/code.js
+++ api/code.js
@@ -1,11 +1,24 @@
 const request = new XMLHttpRequest()
 
+
+
+
+function draw(data){
+  var newDiv=document.createElement("div")
+  var newContent=document.createTextNode(`Tittel: ${data.title}`)
+  newDiv.appendChild(newContent)
+  document.body.appendChild(newDiv)
+}
+
+
+
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function(){
     var data = JSON.parse(this.response)
 
     if (request.status >= 200 && request.status < 400) {
       data.forEach((movie) => {
+        draw(movie)
         console.log(movie)
       })
     } else {
